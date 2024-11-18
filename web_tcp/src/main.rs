5d641cb6e3d4e6d9d158b0080c3f04fd8dd407dd
duanSiYuan <ssy4240@sina.com>
2024-11-18 20:47:42 +0800

    muti thread

---
+++ web_tcp/src/main.rs
@@ -0,0 +1,44 @@
+use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::fs;
+use web_tcp::ThreadPool;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        pool.excute(|| {
+            handle_connection(stream);
+        });
+       
+    
+    }
+}
+
+fn handle_connection(mut stream: TcpStream){
+    let mut buffer  = [0;1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    let (status_line ,filename) = if buffer.starts_with(get) {
+         ("HTTP/1.1 200 OK\r\n\r\n","index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n","404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap_or_else(|_| {
+        println!("Error reading file: {}", filename);
+        String::from("File not found!")
+    });
+
+    let response = format!("{}{}",status_line,contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap(); // 阻塞至流写入成功
+    
+}
