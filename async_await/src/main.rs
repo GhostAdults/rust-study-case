7ef419f6c8ced6d4ee087f5bd81baccb2f9921a7
duanSiYuan <ssy4240@sina.com>
2024-11-21 15:40:40 +0800

    gitignore

--- async_await/src/main.rs
+++ async_await/src/main.rs
@@ -1,4 +1,4 @@
-use std::future::Future;
+use std::{future::{self, Future}, time::Duration};
 
 use trpl::{Either, Html};
 
@@ -11,19 +11,60 @@ async fn page_title(url: &str) -> (&str, Option<String>) {
 }
 
 fn main() {
-    let args: Vec<String> = std::env::args().collect();
+    let mut s = String::from("hello");
+    println!("{s}");
+    // let args: Vec<String> = std::env::args().collect();
+    // trpl::run(async {
+    //     // let url = &args[1];
+    //     let title_fut_1 = page_title(&args[1]);
+    //     let title_fut_2 = page_title(&args[2]);
+    //     let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
+    //         Either::Left(left) => left,
+    //         Either::Right(right) => right,
+    //     };
+    //     match maybe_title {
+    //         Some(title) => println!("title is {}", title),
+    //         None => println!("{url} had no title"),
+    //     }
+    // })
     trpl::run(async {
-        // let url = &args[1];
+        let (tx, mut rx) = trpl::channel(); // mpsc 异步版本
 
-        let title_fut_1 = page_title(&args[1]);
-        let title_fut_2 = page_title(&args[2]);
-        let (url, maybe_title) = match trpl::race(title_fut_1, title_fut_2).await {
-            Either::Left(left) => left,
-            Either::Right(right) => right,
+        let tx1 = tx.clone();
+        let tx1_fut = async move {
+            let vals = vec![
+                String::from("hi"),
+                String::from("from"),
+                String::from("the"),
+                String::from("future"),
+            ];
+
+            for val in vals {
+                tx1.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
         };
-        match maybe_title {
-            Some(title) => println!("title is {}", title),
-            None => println!("{url} had no title"),
-        }
-    })
+
+        let tx_fut = async move {
+            let vals = vec![
+                String::from("more"),
+                String::from("messages"),
+                String::from("for"),
+                String::from("you"),
+            ];
+
+            for val in vals {
+                tx.send(val).unwrap();
+                trpl::sleep(Duration::from_millis(500)).await;
+            }
+        };
+        let rx_fut = async {
+            while let Some(value) = rx.recv().await {
+                println!("got {}", value)
+            }
+        };
+        // let futures = vec![tx1_fut,rx_fut,tx_fut];
+        // trpl::join_all(futures).await;
+        trpl::join(tx_fut,tx1_fut).await
+    });
 }
