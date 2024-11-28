40675881f4de72f892b9e8cb539ae22cd6d351bf
GhostAdults <ssy4240@sina.com>
2024-11-28 23:38:59 +0800

    添加字体

--- bevy_game/src/theme/mod.rs
+++ bevy_game/src/theme/mod.rs
@@ -1,17 +1,26 @@
 use bevy::prelude::*;
+mod interaction;
 pub mod palette;
 mod widget;
-mod interaction;
 
 #[allow(unused_imports)]
 pub mod prelude {
     pub use super::{
+        interaction::{InteractionPalette, OnPress},
         palette as ui_palette,
-        widget::{Containers as _ , Widget as _},
-        interaction::{InteractionPalette,OnPress},
+        widget::{Containers as _, Widget as _},
+        UiFont,
     };
 }
 
+#[derive(Resource, Debug)]
+pub struct UiFont(Handle<Font>);
+
 pub fn plugin(app: &mut App) {
-       
+    app.add_systems(Startup, setup_fonts);
+}
+
+fn setup_fonts(mut commands: Commands, asset_server: Res<AssetServer>) {
+    let font: Handle<Font> = asset_server.load(palette::FONT_FAMILY_MISANS);
+    commands.insert_resource(UiFont(font)); //加入为一个句柄
 }
